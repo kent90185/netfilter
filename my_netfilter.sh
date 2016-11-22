e5ac459ab582fd34e87949ea07e31ee39d884bc3
kent Yeh <kent90185@hotmail.com>
2016-11-22 23:23:21 +0800

    enhance my_netfilter.sh error handler

--- my_netfilter.sh
+++ my_netfilter.sh
@@ -13,11 +13,27 @@ function show_help()
     echo " Usage: $0 [-h] [--help] [-srcip] [-destip] [-protocol]"
     echo " Version $SCRIPT_VERSION, by Riverwind Yeh"
     echo " "
-    echo " -h   --help		:this help"
+    echo " -h   --help		: this help"
     echo " -srcip  <IP>		: show source IP go through prerouting hook "
     echo " -destip <IP>		: show destination IP go through prerouting hook "
     echo " -protocol <TCP/UDP> 	: show TCP or UDP packet go through prerouting hook "
     echo " "
+}
+
+function parse_ip()
+{
+    local  input_ip=$1
+    local  valid=1
+    if [[ $input_ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        input_ip=($input_ip)
+        IFS=$OIFS
+        [[ ${input_ip[0]} -le 255 && ${input_ip[1]} -le 255 \
+            && ${input_ip[2]} -le 255 && ${input_ip[3]} -le 255 ]]
+        valid=$?
+    fi  
+    return $valid
 }
 
 function parse_args()
@@ -28,25 +44,37 @@ function parse_args()
 
     case $key in
     
-	-srcip)
-        echo "$1" > ${DEBUGFS_DIR}/srcip
-		cat ${DEBUGFS_DIR}/srcip_search
+	-srcip|-s) 
+		if parse_ip $1; then
+        	echo "$1" > ${DEBUGFS_DIR}/srcip
+			cat ${DEBUGFS_DIR}/srcip_search
+		else
+			echo "Bad IP address."
+		fi
 		exit 0
         ;;
 	
-	-destip)
-		echo "$1" > ${DEBUGFS_DIR}/destip
-        cat ${DEBUGFS_DIR}/destip_search
+	-destip|-d)
+		if parse_ip $1; then
+			echo "$1" > ${DEBUGFS_DIR}/destip
+        	cat ${DEBUGFS_DIR}/destip_search
+		else
+			echo "Bad IP address."
+		fi
 		exit 0
 		;;
 
-	-protocol)
-		echo "$1" > ${DEBUGFS_DIR}/protocol
-        cat ${DEBUGFS_DIR}/protocol_search
+	-protocol|-p)
+		if [ "$1" == "TCP" ] || [ "$1" == "UDP" ];then
+			echo "$1" > ${DEBUGFS_DIR}/protocol
+			cat ${DEBUGFS_DIR}/protocol_search
+		else
+			echo "Bad protocol"
+		fi
 		exit 0
 		;;
 
-	-h|--help)
+	-h|--help|?)
 		show_help
 		exit 0
 		;;
